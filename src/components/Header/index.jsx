b020a2419f360090304af785d81118a8bdd32dac
Ash Coles <ashley@acoles.co.uk>
2024-06-24 15:02:46 +0100

    header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="py-48 text-center bg-slate-200">
+            <h1 className="text-5xl">Welcome to iO Academy</h1>
+        </header>
+    )
+}
+
+export default Header
